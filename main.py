c9e7c95f0dfc2faa2962ecdd2e3afaf4467513ad
Alexey Koltovskiy <113173214+GolfCharlie762@users.noreply.github.com>
2025-04-29 13:36:53 +0300

    Create main.py
    added main.py with functions implementation

---
+++ main.py
@@ -0,0 +1,32 @@
+from area import Circle, Triangle, calculate_area, is_right_angled
+
+def main():
+    print()
+    
+    try:
+        radius = 5
+        circle = Circle(radius)
+        print(f"Площадь круга с радиусом {radius}: {calculate_area(circle):.2f}")
+        print(f"Является ли круг прямоугольным? {'Да' if is_right_angled(circle) else 'Нет'}")
+    except ValueError as e:
+        print(f"Ошибка при создании круга: {e}")
+    
+    print()
+    
+    try:
+        a, b, c = 3, 4, 5
+        triangle = Triangle(a, b, c)
+        print(f"Площадь треугольника со сторонами {a}, {b}, {c}: {calculate_area(triangle):.2f}")
+        print(f"Является ли треугольник прямоугольным? {'Да' if is_right_angled(triangle) else 'Нет'}")
+    except ValueError as e:
+        print(f"Ошибка при создании треугольника: {e}")
+    
+    print()
+    
+    try:
+        invalid_triangle = Triangle(1, 2, 5)
+    except ValueError as e:
+        print(f"ПОшибка, треугольник не может существовать: {e}")
+
+if __name__ == "__main__":
+    main()
